07addb091f108994c8646c516bd1d247b24dd694
Ashiqul Islam <shouravnkt@gmail.com>
2023-12-06 22:45:51 +0600

    lesson 3 source code push

---
+++ src/components/App.jsx
@@ -0,0 +1,10 @@
+export default function App() {
+  return (
+    <>
+      <h2>Hello MotherFucker</h2>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nulla, dolor.
+      </p>
+    </>
+  );
+}
